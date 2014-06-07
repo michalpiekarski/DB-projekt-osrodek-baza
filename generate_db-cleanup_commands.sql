3b70cbc5b3bf0dd7b6ea08e5374f7984f4425003
Michał Piekarski <michal.piekarski@icloud.com>
2014-06-07 20:09:03 +0200

    Plik do generacji komend na potrzeby czyszczenia bazy danych zaktualizowany - usunięte niepotrzebne polecenia.

--- generate_db-cleanup_commands.sql
+++ generate_db-cleanup_commands.sql
@@ -7,8 +7,4 @@
 /* Left/Right Enclosure: */ /* Leave both blank - no enclosures */
 /* Change the exproted file extention to .sql */
 
-select 'DROP TABLE ' || table_name || ' CASCADE CONSTRAINTS PURGE;' COMMAND from user_tables
-UNION ALL
-select 'DROP INDEX ' || index_name || ';' COMMAND from user_indexes
-UNION ALL
-select 'DROP TRIGGER ' || trigger_name || ';' COMMAND from user_triggers;
+select 'DROP TABLE ' || table_name || ' CASCADE CONSTRAINTS PURGE;' COMMAND from user_tables;
