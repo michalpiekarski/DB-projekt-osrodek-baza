6ec4fd08d682e498a384a0de5bd199069f3219df
Michał Piekarski <michal.piekarski@icloud.com>
2014-06-07 20:05:46 +0200

    Plik do generacji komend na potrzeby czyszczenia bazy danych uzupełniony o instrukcje.

--- generate_db-cleanup_commands.sql
+++ generate_db-cleanup_commands.sql
@@ -1,5 +1,11 @@
 
 /* AFTER RUNNING THIS SCRIPT EXPORT QUERY RESULTS AS SPACE DELIMITED .SQL FILE AND RUN IT */
+/* Export parameters: */
+/* Format: delimites */
+/* Header: unchecked */
+/* Delimiter:  */ /* <-- Space character - not visible here */
+/* Left/Right Enclosure: */ /* Leave both blank - no enclosures */
+/* Change the exproted file extention to .sql */
 
 select 'DROP TABLE ' || table_name || ' CASCADE CONSTRAINTS PURGE;' COMMAND from user_tables
 UNION ALL
